18efdf55dfab28f500d427888492a236d7bfdf3a
Cairath <Cairath@users.noreply.github.com>
2020-01-20 22:58:07 +0100

    Example 'Hello world' mod

---
+++ examples/ONI Hello World Mod/ONIMod/Patches.cs
@@ -0,0 +1,30 @@
+﻿using Harmony;
+
+namespace ONIMod
+{
+	public class Patches
+	{
+		public static class Mod_OnLoad
+		{
+			public static void OnLoad()
+			{
+				Debug.Log("Hello world!");
+			}
+		}
+
+		[HarmonyPatch(typeof(Db))]
+		[HarmonyPatch("Initialize")]
+		public class Db_Initialize_Patch
+		{
+			public static void Prefix()
+			{
+				Debug.Log("I execute before Db.Initialize!");
+			}
+
+			public static void Postfix()
+			{
+				Debug.Log("I execute after Db.Initialize!");
+			}
+		}
+	}
+}
